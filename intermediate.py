74974a0e255a4a7c5fab2fc26e3aeedf2c4208e7
Luke <Luke@lukes-mbp.home>
2021-02-19 11:41:59 +0000

    added cache

--- intermediate.py
+++ intermediate.py
@@ -2,6 +2,7 @@ import requests
 import json
 import requests_cache
 import time
+from IPython.core.display import clear_output
 
 requests_cache.install_cache()
 
@@ -52,5 +53,48 @@ def jprint(obj):
     print(text)
 
 # jprint(r.json())
-jprint(r.json()['artists']['@attr'])
+# jprint(r.json()['artists']['@attr'])
 
+
+
+
+
+
+responses = []
+
+page = 1
+total_pages = 99999 # this is just a dummy number so the loop starts
+
+while page <= total_pages:
+    payload = {
+        'method': 'chart.gettopartists',
+        'limit': 500,
+        'page': page
+    }
+
+    # print some output so we can see the status
+    print("Requesting page {}/{}".format(page, total_pages))
+    # clear the output to make things neater
+    clear_output(wait = True)
+
+    # make the API call
+    response = lastfm_get(payload)
+
+    # if we get an error, print the response and halt the loop
+    if response.status_code != 200:
+        print(response.text)
+        break
+
+    # extract pagination info
+    page = int(response.json()['artists']['@attr']['page'])
+    total_pages = int(response.json()['artists']['@attr']['totalPages'])
+
+    # append response
+    responses.append(response)
+
+    # if it's not a cached result, sleep
+    if not getattr(response, 'from_cache', False):
+        time.sleep(0.25)
+
+    # increment the page number
+    page += 1
