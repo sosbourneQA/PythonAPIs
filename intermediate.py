847050f0aae5708e459a41f4afe0aafcd50714fd
Luke <Luke@lukes-mbp.home>
2021-02-24 18:00:20 +0000

    added methods to intermediate API challenge

--- intermediate.py
+++ intermediate.py
@@ -3,7 +3,11 @@ import json
 import requests_cache
 import time
 from IPython.core.display import clear_output
+import pandas as pd
+from tqdm import tqdm
 
+
+tqdm.pandas()
 requests_cache.install_cache()
 
 # headers = {
@@ -71,11 +75,14 @@ while page <= total_pages:
         'limit': 500,
         'page': page
     }
-
+    if page == 4378:
+        page +=1
+        continue
     # print some output so we can see the status
     print("Requesting page {}/{}".format(page, total_pages))
     # clear the output to make things neater
     clear_output(wait = True)
+
 
     # make the API call
     response = lastfm_get(payload)
@@ -98,3 +105,91 @@ while page <= total_pages:
 
     # increment the page number
     page += 1
+
+
+
+
+
+
+
+# r0 = responses[0]
+# r0_json = r0.json()
+# r0_artists = r0_json['artists']['artist']
+# r0_df = pd.DataFrame(r0_artists)
+# print(r0_df.head())
+
+
+
+
+
+frames = [pd.DataFrame(r.json()['artists']['artist']) for r in responses]
+artists = pd.concat(frames)
+print(artists.head())
+
+
+
+artists = artists.drop('image', axis=1)
+print(artists.head())
+
+
+
+print(artists.info())
+print(artists.describe())
+
+
+
+artist_counts = [len(r.json()['artists']['artist']) for r in responses]
+print(pd.Series(artist_counts).value_counts())
+
+
+
+
+print(artist_counts[:50])
+
+
+
+
+
+artists = artists.drop_duplicates().reset_index(drop=True)
+print(artists.describe())
+
+
+
+
+r = lastfm_get({
+    'method': 'artist.getTopTags',
+    'artist':  'Lana Del Rey'
+})
+
+jprint(r.json())
+
+
+tags = [t['name'] for t in r.json()['toptags']['tag'][:3]]
+
+print(', '.join(tags))
+
+
+
+
+def lookup_tags(artist):
+    response = lastfm_get({
+        'method': 'artist.getTopTags',
+        'artist':  artist
+    })
+
+    # if there's an error, just return nothing
+    if response.status_code != 200:
+        return None
+
+    # extract the top three tags and turn them into a string
+    tags = [t['name'] for t in response.json()['toptags']['tag'][:3]]
+    tags_str = ', '.join(tags)
+
+    # rate limiting
+    if not getattr(response, 'from_cache', False):
+        time.sleep(0.25)
+
+    return tags_str
+
+
+print(lookup_tags("Billie Eilish"))
